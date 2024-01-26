e71c04d91836b5c2718dedb3c51c49c7511eefa9
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-26 10:34:08 +0500

    Typescripr CLI project Oop
    Typescripr CLI project Object O reinted Programm

---
+++ Oop.ts
@@ -0,0 +1,60 @@
+import inquirer from "inquirer";
+
+class Client{
+    name: string
+     constructor(n:string){
+        this.name = n
+     }
+}
+
+class Person{
+    clients:Client[]=[]
+
+    addClient(obj: Client){
+        this.clients.push(obj);
+    }
+}
+
+const persons = new Person();
+const programmStrat = async(persons: Person) => {
+do{
+
+ console.log("Welcome Guest")
+ const ans = await inquirer.prompt({
+        type: "list",
+        message: "Ap kiss say bat Karna Chahaingay...",
+        name: "select",
+        choices: ["khud say:Self", "Client"],
+    });
+if(ans.select == "khud say:Self") {
+        console.log(`Hello M Talking with Myself`);
+        console.log(`Now I am Fine`);
+    }
+    if(ans.select == "Client") {
+      const ans = await inquirer.prompt({
+        type: "input",
+        message: "Ap ko kis Client say baat karni hay.",
+        name: "Client",
+      });
+
+      const client = persons.clients.find(val => val.name == ans.Client)
+
+      if (!client){
+       const name = new Client(ans.Client)
+       persons.addClient(name);
+
+
+       console.log(`Hello i am ${name.name}, Now I am Fine`)
+       console.log(persons.clients);
+      }
+
+      if(client){
+        console.log(`Hello i am ${client.name}, Now I am Fine...........`)
+       console.log(persons.clients);
+      }
+    }
+    }while(true)
+    
+};
+
+programmStrat(persons)
